f90ba374cbd9c20487efa3f9740165469d52704f
AndrewDawnbringer <andrew.dawnbringer@gmail.com>
2022-06-14 08:14:44 +0200

    route and controller setup

---
+++ app/Http/Controllers/CalcController.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class CalcController extends BaseController {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    function __construct () {
+        // output: 470 (370 alappont + 100 többletpont)
+        $this->exampleData = [
+            'valasztott-szak' => [
+                'egyetem' => 'ELTE',
+                'kar' => 'IK',
+                'szak' => 'Programtervező informatikus',
+            ],
+            'erettsegi-eredmenyek' => [
+                [
+                    'nev' => 'magyar nyelv és irodalom',
+                    'tipus' => 'közép',
+                    'eredmeny' => '70%',
+                ],
+                [
+                    'nev' => 'történelem',
+                    'tipus' => 'közép',
+                    'eredmeny' => '80%',
+                ],
+                [
+                    'nev' => 'matematika',
+                    'tipus' => 'emelt',
+                    'eredmeny' => '90%',
+                ],
+                [
+                    'nev' => 'angol nyelv',
+                    'tipus' => 'közép',
+                    'eredmeny' => '94%',
+                ],
+                [
+                    'nev' => 'informatika',
+                    'tipus' => 'közép',
+                    'eredmeny' => '95%',
+                ],
+            ],
+            'tobbletpontok' => [
+                [
+                    'kategoria' => 'Nyelvvizsga',
+                    'tipus' => 'B2',
+                    'nyelv' => 'angol',
+                ],
+                [
+                    'kategoria' => 'Nyelvvizsga',
+                    'tipus' => 'C1',
+                    'nyelv' => 'német',
+                ],
+            ],
+        ];
+
+        // output: 476 (376 alappont + 100 többletpont)
+        $this->exampleData1 = [
+            'valasztott-szak' => [
+                'egyetem' => 'ELTE',
+                'kar' => 'IK',
+                'szak' => 'Programtervező informatikus',
+            ],
+            'erettsegi-eredmenyek' => [
+                [
+                    'nev' => 'magyar nyelv és irodalom',
+                    'tipus' => 'közép',
+                    'eredmeny' => '70%',
+                ],
+                [
+                    'nev' => 'történelem',
+                    'tipus' => 'közép',
+                    'eredmeny' => '80%',
+                ],
+                [
+                    'nev' => 'matematika',
+                    'tipus' => 'emelt',
+                    'eredmeny' => '90%',
+                ],
+                [
+                    'nev' => 'angol nyelv',
+                    'tipus' => 'közép',
+                    'eredmeny' => '94%',
+                ],
+                [
+                    'nev' => 'informatika',
+                    'tipus' => 'közép',
+                    'eredmeny' => '95%',
+                ],
+                [
+                    'nev' => 'fizika',
+                    'tipus' => 'közép',
+                    'eredmeny' => '98%',
+                ],
+            ],
+            'tobbletpontok' => [
+                [
+                    'kategoria' => 'Nyelvvizsga',
+                    'tipus' => 'B2',
+                    'nyelv' => 'angol',
+                ],
+                [
+                    'kategoria' => 'Nyelvvizsga',
+                    'tipus' => 'C1',
+                    'nyelv' => 'német',
+                ],
+            ],
+        ];
+
+        // output: hiba, nem lehetséges a pontszámítás a kötelező érettségi tárgyak hiánya miatt
+        $this->exampleData2 = [
+            'valasztott-szak' => [
+                'egyetem' => 'ELTE',
+                'kar' => 'IK',
+                'szak' => 'Programtervező informatikus',
+            ],
+            'erettsegi-eredmenyek' => [
+                [
+                    'nev' => 'matematika',
+                    'tipus' => 'emelt',
+                    'eredmeny' => '90%',
+                ],
+                [
+                    'nev' => 'angol nyelv',
+                    'tipus' => 'közép',
+                    'eredmeny' => '94%',
+                ],
+                [
+                    'nev' => 'informatika',
+                    'tipus' => 'közép',
+                    'eredmeny' => '95%',
+                ],
+            ],
+            'tobbletpontok' => [
+                [
+                    'kategoria' => 'Nyelvvizsga',
+                    'tipus' => 'B2',
+                    'nyelv' => 'angol',
+                ],
+                [
+                    'kategoria' => 'Nyelvvizsga',
+                    'tipus' => 'C1',
+                    'nyelv' => 'német',
+                ],
+            ],
+        ];
+
+        // output: hiba, nem lehetséges a pontszámítás a magyar nyelv és irodalom tárgyból elért 20% alatti eredmény miatt
+        $this->exampleData3 = [
+            'valasztott-szak' => [
+                'egyetem' => 'ELTE',
+                'kar' => 'IK',
+                'szak' => 'Programtervező informatikus',
+            ],
+            'erettsegi-eredmenyek' => [
+                [
+                    'nev' => 'magyar nyelv és irodalom',
+                    'tipus' => 'közép',
+                    'eredmeny' => '15%',
+                ],
+                [
+                    'nev' => 'történelem',
+                    'tipus' => 'közép',
+                    'eredmeny' => '80%',
+                ],
+                [
+                    'nev' => 'matematika',
+                    'tipus' => 'emelt',
+                    'eredmeny' => '90%',
+                ],
+                [
+                    'nev' => 'angol nyelv',
+                    'tipus' => 'közép',
+                    'eredmeny' => '94%',
+                ],
+                [
+                    'nev' => 'informatika',
+                    'tipus' => 'közép',
+                    'eredmeny' => '95%',
+                ],
+            ],
+            'tobbletpontok' => [
+                [
+                    'kategoria' => 'Nyelvvizsga',
+                    'tipus' => 'B2',
+                    'nyelv' => 'angol',
+                ],
+                [
+                    'kategoria' => 'Nyelvvizsga',
+                    'tipus' => 'C1',
+                    'nyelv' => 'német',
+                ],
+            ],
+        ];
+    }
+
+    
+}
+
+
