06f0513c3819828b84a64892d166eeb89055faef
AndrewDawnbringer <andrew.dawnbringer@gmail.com>
2022-06-14 08:31:57 +0200

    testing view

--- resources/views/pointcalc.blade.php
+++ resources/views/pointcalc.blade.php
@@ -22,9 +22,27 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <div class="container">
-                <p class="text-light">Hello {{ $name }}!</p>
+        
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+
+                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+
+                    <div class="grid grid-cols-1 md:grid-cols-2">
+                        <div class="p-6">
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                    <p>Hello {{ $name }}!<p>    
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
             </div>
+
         </div>
     </body>
 </html>
